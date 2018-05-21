9c4b3c2aeea8b2b31b3ed45155096d963e0c27ab
pwcong <pwcong@foxmail.com>
2018-05-21 23:31:43 +0800

    [+] init frontend framework

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
 	"strconv"
@@ -15,7 +14,6 @@ import (
 	"github.com/pwcong/ablog/model"
 	"github.com/pwcong/ablog/router"
 	"github.com/pwcong/ablog/utils"
-	"github.com/pwcong/ablog/view"
 )
 
 func initMiddlewares(e *echo.Echo, conf *config.Config) {
@@ -77,9 +75,9 @@ func main() {
 	}
 
 	// 视图渲染引擎
-	e.Renderer = &view.Renderer{
-		Templates: template.Must(template.ParseGlob("view/*.html")),
-	}
+	// e.Renderer = &view.Renderer{
+	// 	Templates: template.Must(template.ParseGlob("view/*.html")),
+	// }
 
 	// 初始化中间件
 	initMiddlewares(e, &conf)
